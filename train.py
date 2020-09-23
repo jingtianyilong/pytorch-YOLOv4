2f33346b0ed2ee73f285789aee2b750a1e66e2a9
ZIJIGUO <zijie.guo@daimler.com>
2020-09-23 11:43:04 +0800

    add anchor box calc before train. Might be optimized using sklearn.

--- train.py
+++ train.py
@@ -11,13 +11,14 @@
 
 '''
 import time
+import random
 import logging
 import os, sys, math
 import argparse
 import pprint
 from collections import deque
 import datetime
-
+import pdb
 import cv2
 from tqdm import tqdm
 import numpy as np
@@ -34,6 +35,7 @@ from config import update_config
 from models import Yolov4
 from tool.darknet2pytorch import Darknet
 from utils.yolo_loss import Yolo_loss, bboxes_iou
+from utils.utils import init_seed
 from tool.tv_reference.utils import collate_fn as val_collate
 from tool.tv_reference.coco_utils import convert_to_coco_api
 from tool.tv_reference.coco_eval import CocoEvaluator
@@ -62,9 +64,9 @@ def train(model, device, config, tb_log_dir, epochs=5, batch_size=1, save_cp=Tru
     n_val = len(val_dataset)
 
     train_loader = DataLoader(train_dataset, batch_size=config.batch // config.subdivisions, shuffle=True,
-                              num_workers=8, pin_memory=True, drop_last=True, collate_fn=collate)
+                              num_workers=16, pin_memory=True, drop_last=True, collate_fn=collate)
 
-    val_loader = DataLoader(val_dataset, batch_size=config.batch // config.subdivisions, shuffle=True, num_workers=8,
+    val_loader = DataLoader(val_dataset, batch_size=config.batch // config.subdivisions, shuffle=True, num_workers=16,
                             pin_memory=True, drop_last=True, collate_fn=val_collate)
 
     writer = SummaryWriter(log_dir=tb_log_dir,
@@ -73,7 +75,7 @@ def train(model, device, config, tb_log_dir, epochs=5, batch_size=1, save_cp=Tru
     # writer.add_images('legend',
     #                   torch.from_numpy(train_dataset.label2colorlegend2(cfg.DATA_CLASSES).transpose([2, 0, 1])).to(
     #                       device).unsqueeze(0))
-    max_itr = config.TRAIN_EPOCHS * n_train
+    max_itr = epochs * n_train
     # global_step = cfg.TRAIN_MINEPOCH * n_train
     global_step = 0
     logging.info(f'''Starting training:
@@ -81,7 +83,7 @@ def train(model, device, config, tb_log_dir, epochs=5, batch_size=1, save_cp=Tru
         Batch size:      {config.batch}
         Subdivisions:    {config.subdivisions}
         Learning rate:   {config.learning_rate}
-        Steps:           {max_itr%config.subdivisions}
+        Steps:           {max_itr//config.subdivisions}
         Training size:   {n_train}
         Validation size: {n_val}
         Checkpoints:     {save_cp}
@@ -93,14 +95,16 @@ def train(model, device, config, tb_log_dir, epochs=5, batch_size=1, save_cp=Tru
         Pretrained:
     ''')
     def burnin_schedule(i):
-        if i < config.burn_in:
-            factor = pow(i / config.burn_in, 4)
-        elif i < int(max_itr*0.8)% config.subdivisions:
+        # pdb.set_trace()
+        if i < n_train//config.batch:
+            factor = pow(i / (n_train//config.batch), 4)
+        elif i < int(max_itr*0.8//config.batch):
             factor = 1.0
-        elif i < int(max_itr*0.9)% config.subdivisions:
+        elif i < int(max_itr*0.9//config.batch):
             factor = 0.1
         else:
             factor = 0.01
+        # print(factor)
         return factor
     
     if config.TRAIN_OPTIMIZER.lower() == 'adam':
@@ -129,7 +133,7 @@ def train(model, device, config, tb_log_dir, epochs=5, batch_size=1, save_cp=Tru
         epoch_loss = 0
         epoch_step = 0
 
-        with tqdm(total=n_train, desc=f'Epoch {epoch + 1}/{epochs}', unit='img', ncols=50) as pbar:
+        with tqdm(total=n_train, desc=f'Epoch {epoch + 1}/{epochs}', ncols=80) as pbar:
             for i, batch in enumerate(train_loader):
                 global_step = global_step + 1
                 epoch_step = epoch_step + 1
@@ -141,7 +145,6 @@ def train(model, device, config, tb_log_dir, epochs=5, batch_size=1, save_cp=Tru
                 # with to
                 bboxes_pred = model(images)
                 loss, loss_xy, loss_wh, loss_obj, loss_cls, loss_l2 = criterion(bboxes_pred, bboxes)
-                # loss = loss / config.subdivisions
                 loss.backward()
 
                 epoch_loss = epoch_loss + loss.item()
@@ -161,9 +164,9 @@ def train(model, device, config, tb_log_dir, epochs=5, batch_size=1, save_cp=Tru
                     # writer.add_scalar('lr', scheduler.get_last_lr()[0], global_step)
                     pbar.set_postfix(**{'loss (batch)': loss.item(), 'loss_xy': loss_xy.item(),
                                         'loss_wh': loss_wh.item(),
-                                        'loss_obj': loss_obj.item(),
+                                        # 'loss_obj': loss_obj.item(),
                                         'loss_cls': loss_cls.item(),
-                                        'loss_l2': loss_l2.item(),
+                                        # 'loss_l2': loss_l2.item(),
                                         'lr': scheduler.get_last_lr()[0]
                                         })
                     logging.debug('Train step_{:.06f}: loss : {:.06f},loss xy : {:.06f},loss wh : {:.06f},'
@@ -216,7 +219,6 @@ def train(model, device, config, tb_log_dir, epochs=5, batch_size=1, save_cp=Tru
                         os.remove(model_to_remove)
                     except:
                         logging.info(f'failed to remove {model_to_remove}')
-
     writer.close()
 
 
@@ -232,7 +234,7 @@ def evaluate(model, data_loader, cfg, device, logger=None, **kwargs):
     coco = convert_to_coco_api(data_loader.dataset, bbox_fmt='coco')
     coco_evaluator = CocoEvaluator(coco, iou_types = ["bbox"], bbox_fmt='coco')
     
-    for images, targets in tqdm(data_loader,desc="Validation Batch: "):
+    for images, targets in tqdm(data_loader,desc="Validation Batch: ", ncols=80):
         model_input = [[cv2.resize(img, (cfg.w, cfg.h))] for img in images]
         model_input = np.concatenate(model_input, axis=0)
         model_input = model_input.transpose(0, 3, 1, 2)
@@ -335,11 +337,106 @@ def _get_date_str():
     now = datetime.datetime.now()
     return now.strftime('%Y-%m-%d_%H-%M')
 
+def get_anchors(label_file, width_in_cfg, height_in_cfg, num_of_anchor=9):
+    def IOU(x,centroids):
+        similarities = []
+        k = len(centroids)
+        for centroid in centroids:
+            c_w,c_h = centroid
+            w,h = x
+            if c_w>=w and c_h>=h:
+                similarity = w*h/(c_w*c_h)
+            elif c_w>=w and c_h<=h:
+                similarity = w*c_h/(w*h + (c_w-w)*c_h)
+            elif c_w<=w and c_h>=h:
+                similarity = c_w*h/(w*h + c_w*(c_h-h))
+            else: #means both w,h are bigger than c_w and c_h respectively
+                similarity = (c_w*c_h)/(w*h)
+            similarities.append(similarity) # will become (k,) shape
+        return np.array(similarities) 
+
+    def avg_IOU(X,centroids):
+        n,d = X.shape
+        sum = 0.
+        for i in range(X.shape[0]):
+            #note IOU() will return array which contains IoU for each centroid and X[i] // slightly ineffective, but I am too lazy
+            sum+= max(IOU(X[i],centroids)) 
+        return sum/n    
+    
+    def kmeans(X,centroids,eps):
+        N = X.shape[0]
+        iterations = 0
+        k,dim = centroids.shape
+        prev_assignments = np.ones(N)*(-1)    
+        iter = 0
+        old_D = np.zeros((N,k))
+
+        while True:
+            D = []
+            iter+=1           
+            for i in range(N):
+                d = 1 - IOU(X[i],centroids)
+                # pdb.set_trace()
+                D.append(d)
+            
+            D = np.array(D)
+            print("iter {}: dists = {}".format(iter,np.sum(np.abs(old_D-D))))
+                
+            #assign samples to centroids 
+            assignments = np.argmin(D,axis=1)
+            
+            if (assignments == prev_assignments).all() :
+                print("Centroids = ",centroids)
+                anchors = centroids.copy()
+                print(anchors.shape)
+                for i in range(anchors.shape[0]):
+                    anchors[i][0]*=width_in_cfg/32.
+                    anchors[i][1]*=height_in_cfg/32.
+                anchors.sort()
+                return list(np.concatenate(anchors))
+
+            #calculate new centroids
+            centroid_sums=np.zeros((k,dim),np.float)
+            for i in range(N):
+                centroid_sums[assignments[i]]+=X[i]        
+            for j in range(k):            
+                centroids[j] = centroid_sums[j]/(np.sum(assignments==j))
+            
+            prev_assignments = assignments.copy()     
+            old_D = D.copy()  
+    
+    f = open(label_file)        
+    lines = [line.rstrip('\n') for line in f.readlines()]
+    annotation_dims = []
+    # pdb.set_trace()
+    for line in lines:
+        line = line.rstrip().split()
+        try:
+            for obj in line[1:]:
+                obj = obj.split(",")
+                annotation_dims.append(tuple(map(float,(obj[2],obj[3]))))
+        except:
+            pass
+    annotation_dims = np.array(annotation_dims)
+    eps = 0.005
+   
+    if num_of_anchor == 0:
+        for num_clusters in range(1,11): #we make 1 through 10 clusters 
+            indices = [ random.randrange(annotation_dims.shape[0]) for i in range(num_clusters)]
+            centroids = annotation_dims[indices]
+            anchor_list = kmeans(annotation_dims,centroids,eps)
+    else:
+        indices = [ random.randrange(annotation_dims.shape[0]) for i in range(num_of_anchor)]
+        centroids = annotation_dims[indices]
+        anchor_list = kmeans(annotation_dims,centroids,eps)
+    return anchor_list
 
 if __name__ == "__main__":
     args = getArgs()
     # import config file and save it to log
     update_config(cfg, args)
+    init_seed(cfg.SEED)
+    anchors = get_anchors(cfg.train_label, cfg.width, cfg.height)
     log_dir = os.path.join("log",os.path.basename(args.config_file)[:-5])
     logging = init_logger(log_dir=log_dir)
     logging.info(pprint.pformat(args))
@@ -350,7 +447,7 @@ if __name__ == "__main__":
     if cfg.use_darknet_cfg:
         model = Darknet(cfg.cfgfile)
     else:
-        model = Yolov4(cfg.anchors, yolov4conv137weight=cfg.pretrained, n_classes=cfg.classes)
+        model = Yolov4(anchors, yolov4conv137weight=cfg.pretrained, n_classes=cfg.classes)
 
     if torch.cuda.device_count() > 1:
         model = torch.nn.DataParallel(model)
